5c18860709cb6875f0f4eca66a8319e54f4de7ec
Jeng-Sheng Yeh 葉正聖 <jsyeh@mail.mcu.edu.tw>
2024-03-25 16:33:19 +0800

    week06

---
+++ week06-3_glm_again/main.cpp
@@ -0,0 +1,29 @@
+#include <GL/glut.h>
+#include "glm.h" ///加這段
+GLMmodel * pmodel = NULL; ///加這段
+void drawmodel(void)
+{
+    if (!pmodel) {
+	pmodel = glmReadOBJ("data/porsche.obj");
+	if (!pmodel) exit(0);
+	glmUnitize(pmodel);
+	glmFacetNormals(pmodel);
+	glmVertexNormals(pmodel, 90.0);
+    }
+
+    glmDraw(pmodel, GLM_SMOOTH | GLM_MATERIAL);
+}
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    drawmodel(); ///glutSolidTeapot( 0.3 );
+    glutSwapBuffers();
+}
+int main(int argc, char*argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week06 glm obj model");
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
